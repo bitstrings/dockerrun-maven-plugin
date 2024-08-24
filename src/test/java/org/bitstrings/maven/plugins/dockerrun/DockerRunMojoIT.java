dfd17c801ab767301ec9f30d8923f0f2df766f87
Pino Silvaggio <pino.silvaggio@gmail.com>
2024-08-24 17:07:01 -0400

    fix: Remove unwanted file header

--- src/test/java/org/bitstrings/maven/plugins/dockerrun/DockerRunMojoIT.java
+++ src/test/java/org/bitstrings/maven/plugins/dockerrun/DockerRunMojoIT.java
@@ -1,17 +1,3 @@
-/*
- *=============================================================================
- *                      THIS FILE AND ITS CONTENTS ARE THE
- *                    EXCLUSIVE AND CONFIDENTIAL PROPERTY OF
- *
- *                          EXPRETIO TECHNOLOGIES, INC.
- *
- * Any unauthorized use of this file or any of its parts, including, but not
- * limited to, viewing, editing, copying, compiling, and distributing, is
- * strictly prohibited.
- *
- * Copyright ExPretio Technologies, Inc., 2024. All rights reserved.
- *=============================================================================
- */
 package org.bitstrings.maven.plugins.dockerrun;
 
 import static com.soebes.itf.jupiter.extension.MavenCLIOptions.NO_TRANSFER_PROGRESS;
